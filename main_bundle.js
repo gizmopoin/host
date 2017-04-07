cef033ac5124228b679b787f7aae127eca37a19f
Gizmopoin <gizmopoin@gmail.com>
2017-04-07 10:31:21 +0700

    Update main_bundle.js
    All-In-One main JavaScrpit file
    - Change no_thumb.png, avatar_45.png, and avatar_55.png link sources
    - Bautifier .JS file for friendly read

--- main_bundle.js
+++ main_bundle.js
@@ -18,13 +18,157 @@ var POSTNAV_NEXT_TEXT = "Next"; // post nav text "next post"
 /*JQUERY PLUGINS*/
 
 // JQuery hover event with timeout by Taufik Nurrohman - https://plus.google.com/108949996304093815163/about
-(function(c){c.fn.hoverTimeout=function(d,e,f,g){return this.each(function(){var a=null,b=c(this);b.hover(function(){clearTimeout(a);a=setTimeout(function(){e.call(b)},d)},function(){clearTimeout(a);a=setTimeout(function(){g.call(b)},f)})})}})(jQuery);
+(function(c) {
+    c.fn.hoverTimeout = function(d, e, f, g) {
+        return this.each(function() {
+            var a = null,
+                b = c(this);
+            b.hover(function() {
+                clearTimeout(a);
+                a = setTimeout(function() {
+                    e.call(b)
+                }, d)
+            }, function() {
+                clearTimeout(a);
+                a = setTimeout(function() {
+                    g.call(b)
+                }, f)
+            })
+        })
+    }
+})(jQuery);
 
 // jquery replacetext plugin https://github.com/cowboy/jquery-replacetext
-(function(e){e.fn.replaceText=function(t,n,r){return this.each(function(){var i=this.firstChild,s,o,u=[];if(i){do{if(i.nodeType===3){s=i.nodeValue;o=s.replace(t,n);if(o!==s){if(!r&&/</.test(o)){e(i).before(o);u.push(i)}else{i.nodeValue=o}}}}while(i=i.nextSibling)}u.length&&e(u).remove()})}})(jQuery);
+(function(e) {
+    e.fn.replaceText = function(t, n, r) {
+        return this.each(function() {
+            var i = this.firstChild,
+                s, o, u = [];
+            if (i) {
+                do {
+                    if (i.nodeType === 3) {
+                        s = i.nodeValue;
+                        o = s.replace(t, n);
+                        if (o !== s) {
+                            if (!r && /</.test(o)) {
+                                e(i).before(o);
+                                u.push(i)
+                            } else {
+                                i.nodeValue = o
+                            }
+                        }
+                    }
+                } while (i = i.nextSibling)
+            }
+            u.length && e(u).remove()
+        })
+    }
+})(jQuery);
 
 // Plugin: SelectNav.js ~ url: https://github.com/lukaszfiszer/selectnav.js
-window.selectnav=function(){"use strict";var e=function(e,t){function c(e){var t;if(!e)e=window.event;if(e.target)t=e.target;else if(e.srcElement)t=e.srcElement;if(t.nodeType===3)t=t.parentNode;if(t.value)window.location.href=t.value}function h(e){var t=e.nodeName.toLowerCase();return t==="ul"||t==="ol"}function p(e){for(var t=1;document.getElementById("selectnav"+t);t++);return e?"selectnav"+t:"selectnav"+(t-1)}function d(e){a++;var t=e.children.length,n="",l="",c=a-1;if(!t){return}if(c){while(c--){l+=o}l+=" "}for(var v=0;v<t;v++){var m=e.children[v].children[0];if(typeof m!=="undefined"){var g=m.innerText||m.textContent;var y="";if(r){y=m.className.search(r)!==-1||m.parentNode.className.search(r)!==-1?f:""}if(i&&!y){y=m.href===document.URL?f:""}n+='<option value="'+m.href+'" '+y+">"+l+g+"</option>";if(s){var b=e.children[v].children[1];if(b&&h(b)){n+=d(b)}}}}if(a===1&&u){n='<option value="">'+u+"</option>"+n}if(a===1){n='<select class="selectnav" id="'+p(true)+'">'+n+"</select>"}a--;return n}e=document.getElementById(e);if(!e){return}if(!h(e)){return}if(!("insertAdjacentHTML"in window.document.documentElement)){return}document.documentElement.className+=" js";var n=t||{},r=n.activeclass||"active",i=typeof n.autoselect==="boolean"?n.autoselect:true,s=typeof n.nested==="boolean"?n.nested:true,o=n.indent||"?",u=n.label||"Menu",a=0,f=" selected ";e.insertAdjacentHTML("afterend",d(e));var l=document.getElementById(p());if(l.addEventListener){l.addEventListener("change",c)}if(l.attachEvent){l.attachEvent("onchange",c)}return l};return function(t,n){e(t,n)}}();$(document).ready(function(){selectnav('menu-main-nav');selectnav('nav1');});
+window.selectnav = function() {
+    "use strict";
+    var e = function(e, t) {
+        function c(e) {
+            var t;
+            if (!e) e = window.event;
+            if (e.target) t = e.target;
+            else if (e.srcElement) t = e.srcElement;
+            if (t.nodeType === 3) t = t.parentNode;
+            if (t.value) window.location.href = t.value
+        }
+
+        function h(e) {
+            var t = e.nodeName.toLowerCase();
+            return t === "ul" || t === "ol"
+        }
+
+        function p(e) {
+            for (var t = 1; document.getElementById("selectnav" + t); t++);
+            return e ? "selectnav" + t : "selectnav" + (t - 1)
+        }
+
+        function d(e) {
+            a++;
+            var t = e.children.length,
+                n = "",
+                l = "",
+                c = a - 1;
+            if (!t) {
+                return
+            }
+            if (c) {
+                while (c--) {
+                    l += o
+                }
+                l += " "
+            }
+            for (var v = 0; v < t; v++) {
+                var m = e.children[v].children[0];
+                if (typeof m !== "undefined") {
+                    var g = m.innerText || m.textContent;
+                    var y = "";
+                    if (r) {
+                        y = m.className.search(r) !== -1 || m.parentNode.className.search(r) !== -1 ? f : ""
+                    }
+                    if (i && !y) {
+                        y = m.href === document.URL ? f : ""
+                    }
+                    n += '<option value="' + m.href + '" ' + y + ">" + l + g + "</option>";
+                    if (s) {
+                        var b = e.children[v].children[1];
+                        if (b && h(b)) {
+                            n += d(b)
+                        }
+                    }
+                }
+            }
+            if (a === 1 && u) {
+                n = '<option value="">' + u + "</option>" + n
+            }
+            if (a === 1) {
+                n = '<select class="selectnav" id="' + p(true) + '">' + n + "</select>"
+            }
+            a--;
+            return n
+        }
+        e = document.getElementById(e);
+        if (!e) {
+            return
+        }
+        if (!h(e)) {
+            return
+        }
+        if (!("insertAdjacentHTML" in window.document.documentElement)) {
+            return
+        }
+        document.documentElement.className += " js";
+        var n = t || {},
+            r = n.activeclass || "active",
+            i = typeof n.autoselect === "boolean" ? n.autoselect : true,
+            s = typeof n.nested === "boolean" ? n.nested : true,
+            o = n.indent || "?",
+            u = n.label || "Menu",
+            a = 0,
+            f = " selected ";
+        e.insertAdjacentHTML("afterend", d(e));
+        var l = document.getElementById(p());
+        if (l.addEventListener) {
+            l.addEventListener("change", c)
+        }
+        if (l.attachEvent) {
+            l.attachEvent("onchange", c)
+        }
+        return l
+    };
+    return function(t, n) {
+        e(t, n)
+    }
+}();
+$(document).ready(function() {
+    selectnav('menu-main-nav');
+    selectnav('nav1');
+});
 
 // Main Scripts
 $("#LinkList98").each(function() {
